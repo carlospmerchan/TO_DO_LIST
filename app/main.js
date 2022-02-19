ec51080395bd9d528c035ec3bc458531a557b58a
lorenzogo <lorenzo@iMac-de-lorenzo.local>
2022-02-19 19:36:20 +0100

    lorenzo se pone al día

--- app/main.js
+++ app/main.js
@@ -1,81 +1,13 @@
 //----JS-------
 
 
-let form = document.querySelector("#agregar");
+let form = document.querySelector("#btn_add");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("click", (e) => {
+    
     let porHacer = document.querySelector("#tarea");
     console.log(porHacer.value);
     let laLista = document.querySelector("#tareasList");
-    laLista.innerHTML=`<li class="item">${porHacer.value}<button class="btn eliminar">Eliminar</button></li>`;
-
+    laLista.innerHTML+=`<li class="item">${porHacer.value}<button class="btn eliminar"><img class="delete" src="assets/images/trash_icon.svg" alt="trash icon"></button></li>`;
+    porHacer.value = '';
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Definir un array
-
-// let porHacer = ['Escribir un libro','Plantar un árbol','Tener un hijo','Aprobar el LAB'];
-
-// // Aplicarlo a una lista
-
-// let laLista = document.querySelector("#tareasList");
-
-// let contador = 0;
-// let longitud = porHacer.length
-
-// while (contador <= longitud) {
-//     laLista.innerHTML=`<li class="item">${porHacer[contador]}<button class="btn eliminar">Eliminar</button></li>`;
-//     contador++;
-//     return;
-//     console.log(contador);
-// }
-
-
- 
-// Definir un array
-
-// let porHacer = ['Escribir un libro','Plantar un árbol','Tener un hijo','Aprobar el LAB'];
-
-// Aplicarlo a una lista
-
-// let laLista = document.querySelector("#tareasList");
-// let recados = porHacer[1];
-// laLista.innerHTML=`<li class="item">${recados}<button class="btn eliminar">Eliminar</button></li>`;
-
-
-
-
-
-// let form = document.querySelector("#agregar");
-
-// form.addEventListener("submit", (e) => {
-//     let porHacer = document.querySelector("#tarea");
-//     console.log(porHacer.value);
-//     let laLista = document.querySelector("#tareasList");
-//     let li = document.createElement('li');
-//     li.textContent = porHacer.value; 
-//     laLista.appendChild(li);
-
-// });
