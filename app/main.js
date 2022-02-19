c97d9445f404eb4c05643325062add8e78a81939
lorenzogo <lorenzo.go@gmail.com>
2022-02-19 20:14:33 +0100

    cocinado_B

--- app/main.js
+++ app/main.js
@@ -1,13 +1,12 @@
 //----JS-------
 
 
-let form = document.querySelector("#btn_add");
+let form = document.querySelector("#btn_suma");
 
 form.addEventListener("click", (e) => {
     
     let porHacer = document.querySelector("#tarea");
-    console.log(porHacer.value);
     let laLista = document.querySelector("#tareasList");
-    laLista.innerHTML+=`<li class="item">${porHacer.value}<button class="btn eliminar"><img class="delete" src="assets/images/trash_icon.svg" alt="trash icon"></button></li>`;
+    laLista.innerHTML+=`<li class="item">${porHacer.value}<button class="btn_resta"><img class="delete" src="assets/images/trash_icon.svg" alt="trash icon"></button></li>`;
     porHacer.value = '';
 });
