5a2c703f33defba03ffc1a4acc2acc1d5c66ab69
lorenzogo <lorenzo.go@gmail.com>
2022-02-20 15:38:11 +0100

    a_falta_de_eliminar

--- app/main.js
+++ app/main.js
@@ -6,7 +6,25 @@ let form = document.querySelector("#btn_suma");
 form.addEventListener("click", (e) => {
     
     let porHacer = document.querySelector("#tarea");
+    console.log(porHacer.value);
+    
+    if (porHacer.value == false) {
+        alert("Nos ha introducido ninguna tarea");
+        return;
+    };
+
     let laLista = document.querySelector("#tareasList");
-    laLista.innerHTML+=`<li class="item">${porHacer.value}<button class="btn_resta"><img class="delete" src="assets/images/trash_icon.svg" alt="trash icon"></button></li>`;
+    laLista.innerHTML+=`<li class="item">
+        <div class="item_text">${porHacer.value}</div>
+        <button class="btn_resta"><img class="delete" src="assets/images/restar.svg" alt="eliminar"></button>
+    </li>`;
     porHacer.value = '';
+    
 });
+
+
+
+
+
+
+
