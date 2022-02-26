4ad40d8473f37a617044c670927e919a77abd1ea
carlospmerchan <carlosmerchandesign@gmail.com>
2022-02-26 19:53:46 +0100

    delete_item_2

--- app/main.js
+++ app/main.js
@@ -1,39 +1,11 @@
 //----JS-------
 
-// Añadir tareas
+    // Restar tarea de ejemplo
 
-let form = document.querySelector("#btn_suma");
+    let quitar_prev = document.querySelector(".delete");
 
-form.addEventListener("click", (e) => {
+    quitar_prev.addEventListener('click', (e) => {
 
-    let porHacer = document.querySelector("#tarea");
-    console.log(porHacer.value);
-
-
-
-
-    if (porHacer.value == false) {
-        alert("Nos ha introducido ninguna tarea");
-        return;
-    };
-
-
-    let laLista = document.querySelector("#tareasList");
-    laLista.innerHTML += `<li class="item">
-        <div class="item_text">${porHacer.value}</div>
-        <button class="btn_resta"><img class="delete" src="assets/images/restar.svg" alt="eliminar"></button>
-    </li>`;
-
-    porHacer.value = '';
-
-
-    
-
-    // Restar tareas
-
-    let quitar = document.querySelector(".delete");
-
-    quitar.addEventListener('click', (e) => {
         borro = e.target;
 
         let subo_uno = borro.parentNode;
@@ -47,10 +19,78 @@ form.addEventListener("click", (e) => {
     });
 
 
+    // Añadir tareas
 
+    let form = document.querySelector("#btn_suma");
+
+    form.addEventListener("click", (e) => {
+
+        let porHacer = document.querySelector("#tarea");
+        console.log(porHacer.value);
+
+
+        // Avisar de que no se ha introducido ninguna tarea
+
+        if (porHacer.value == false) {
+            alert("Nos ha introducido ninguna tarea");
+            return;
+        };
+
+
+     
+        // Pintar tareas
+
+        let laLista = document.querySelector("#tareasList");
+
+        // Borrar tarea de ejemplo al introducir mi primer item
+
+        let sampleado = laLista.querySelector(".sample");
+        console.log(sampleado);
+        sampleado.classList.add('oculto');
+     
+
+        
+        laLista.innerHTML += `<li class="item">
+            <div class="item_text">${porHacer.value}</div>
+            <button class="btn_resta"><img class="delete" src="assets/images/restar.svg" alt="eliminar"></button>
+        </li>`;
+
+        porHacer.value = '';
+    
+
+    // Restar tareas
+
+    let quitar = document.querySelectorAll(".delete");
+
+    for (let i=0 ; i < quitar.length; i++) {
+
+ 
+
+    quitar[i].addEventListener('click', (e) => {
+
+        borro = e.target;
+
+        let subo_uno = borro.parentNode;
+
+        let subo_dos = subo_uno.parentNode;
+
+        let subo_tres = subo_dos.parentNode;
+
+        subo_tres.removeChild(subo_dos);
+
+    });
+
+
+}
 
 
 });
+
+
+
+
+
+
 
 
 
